eb8bae9b12a6d33117327a1d37a18b4923c608ad
wt854 <wt854@outlook.com>
2020-11-03 18:19:29 +0800

    page 2 completed, email validation and dynamic phone num

--- src/App.js
+++ src/App.js
@@ -9,16 +9,15 @@ import { Result } from './Result';
 function App() {
   return (
     <>
-      <Header>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Step1} />
-            <Route path='/step2' component={Step2} />
-            <Route path='/step3' component={Step3} />
-            <Route path='/result' component={Result} />
-          </Switch>
-        </Router>
-      </Header>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Step1} />
+          <Route path='/step2' component={Step2} />
+          <Route path='/step3' component={Step3} />
+          <Route path='/result' component={Result} />
+        </Switch>
+      </Router>
     </>
   );
 }
